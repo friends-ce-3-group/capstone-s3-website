bed7d00e9d4be8a64ed26b9679f6511eef478f4c
Justin Lim <goodaccoustics@hotmail.com>
2023-11-19 13:49:00 +0800

    initial implementation of captcha

--- src/utilities/Constants.js
+++ src/utilities/Constants.js
@@ -1,6 +1,9 @@
+
+export const AWS_CAPTCHA_INTEGRATION_SCRIPT = "https://ce0088630af3.us-west-2.captcha-sdk.awswaf.com/ce0088630af3/jsapi.js"
+export const AWS_CAPTCHA_KEY = "{AWS_CAPTCHA_KEY}"
+
 //new (these links to be replaced with CloudFront links later)
 export const AWS_API_GATEWAY_UPLOAD_IMAGE_URL = "https://ybqb8ftiy3.execute-api.us-west-2.amazonaws.com/dev/friends-capstone-infra-s3-images/originals/{filename}";
-//https://dae7avsudwv03.cloudfront.net/upload/dev/friends-capstone-infra-s3-images/originals/test.png
 
 //export const AWS_ALB_CRUD_HOST_NAME = "http://friends-capstone-ecs-shared-alb-2059628003.us-west-2.elb.amazonaws.com:5000";
 export const AWS_CF_HOST_NAME = "https://dae7avsudwv03.cloudfront.net";
